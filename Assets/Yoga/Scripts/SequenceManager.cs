0edacf026baf32d9de024b93a9aa14f730c356b6
benjamin.brodwolf <benjamin.brodwolf@students.fhnw.ch>
2020-12-30 17:39:13 +0100

    for- and backward animation

--- Assets/Yoga/Scripts/SequenceManager.cs
+++ Assets/Yoga/Scripts/SequenceManager.cs
@@ -11,31 +11,43 @@ public class SequenceManager : MonoBehaviour
     private GameObject _startButtonUI;
     private GameObject _placeOkButtonUI;
     private GameObject _placingButtonUI;
-    
+    private GameObject _nextButtonUI;
+    private GameObject _backButtonUI;
+
+    public bool isAnimationPlaying;
+    private int countSequence;
     void Start()
     {
+        isAnimationPlaying = false;
+        countSequence = 0;
+        
         _anim = GetComponent<Animator>();
         
         _placingButtonUI = GameObject.FindGameObjectsWithTag("PlacingButton")[0];
-
-        _startButtonUI = FindInActiveObjectByTag("StartButton"); 
         
         _placeOkButtonUI = FindInActiveObjectByTag("PlaceOkButton"); 
         _placeOkButtonUI.SetActive(true);
         _placeOkButtonUI.GetComponent<Button>().onClick.AddListener(PlatzierungOk);
+        
+        _nextButtonUI = FindInActiveObjectByTag("NextButton"); 
+        _nextButtonUI.GetComponent<Button>().onClick.AddListener(NextYogaSequence);
+
+        _backButtonUI = FindInActiveObjectByTag("BackButton"); 
+        _backButtonUI.GetComponent<Button>().onClick.AddListener(BackYogaSequence);
     }
 
     void Update()
     {
- 
+        Debug.Log(GetCurrentClipName());
+        Debug.Log(AnimatorIsPlaying());
     }
 
     public void PlatzierungOk()
     {
-        // Display the Start Button
-        _startButtonUI.SetActive(true);
-        _startButtonUI.GetComponent<Button>().onClick.AddListener(StartButton);
-        
+        // Display the Next Button
+        _nextButtonUI.SetActive(true);
+        _backButtonUI.SetActive(true);
+
         //Disable Scale and Rotate Scripts
         gameObject.GetComponent<rotateController>().enabled = false;
         gameObject.GetComponent<onClickForScaling>().enabled = false;
@@ -44,12 +56,33 @@ public class SequenceManager : MonoBehaviour
         _placeOkButtonUI.SetActive(false);
         _placingButtonUI.SetActive(false);
     }
-    public void StartButton()
+
+
+    public void NextYogaSequence()
     {
-        Debug.Log("Start Button pressed");
-        _anim.SetBool("isStartYogaSequence", true);
+        Debug.Log("Next Sequence Button pressed");
+        _anim.SetTrigger("nextYogaSequenceTrigger");
+        countSequence++;
     }
     
+    public void BackYogaSequence()
+    {
+        Debug.Log("Back Sequence Button pressed");
+        _anim.SetTrigger("backYogaSequenceTrigger");
+        countSequence--;
+    }
+    
+    private bool AnimatorIsPlaying(){
+        return _anim.GetCurrentAnimatorStateInfo(0).length >
+               _anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+    
+    
+    
+    public string GetCurrentClipName(){
+        var clipInfo = _anim.GetCurrentAnimatorClipInfo(0);
+        return clipInfo[0].clip.name;
+    }
     GameObject FindInActiveObjectByTag(string tag)
     {
 
